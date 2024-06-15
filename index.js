328d801672d02f1c0fcc2fbbba96aa362e9c5660
Azaryan8 <165148838+Azaryan8@users.noreply.github.com>
2024-06-15 21:47:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function Contacts(){
+    var options = document.getElementById(`contactsOptions`);
+    if(options.style.display==='none')
+    {options.style.display = 'block';
+} else {
+    options.style.display = 'none'
+}
+}
+
